a0854289d3c2be85d8381ffe7e749f8afda18354
Atakan Yontar <atakanyontar@gmail.com>
2022-05-19 02:39:11 +0300

    feat: two hcsr04 sensor v2

--- asan_car/hc-sr04.py
+++ asan_car/hc-sr04.py
@@ -10,7 +10,7 @@ R_ECHO = 7
 L_TRIG = 26
 L_ECHO = 19
 
-print("HC-SR04 mesafe sensoru")
+print("HC-SR04 distance measurement in progress")
 
 GPIO.setup(R_TRIG,GPIO.OUT)
 GPIO.setup(R_ECHO,GPIO.IN)
@@ -20,16 +20,14 @@ GPIO.setup(L_ECHO,GPIO.IN)
 
 while True:
 
+    # Right sensor begin
     GPIO.output(R_TRIG, False)
-    GPIO.output(L_TRIG, False)
     print("Olculuyor...")
-    time.sleep(2)
+    time.sleep(1)
 
     GPIO.output(R_TRIG, True)
-    GPIO.output(L_TRIG, True)
     time.sleep(0.00001)
     GPIO.output(R_TRIG, False)
-    GPIO.output(L_TRIG, False)
 
     while GPIO.input(R_ECHO)==0:
         right_pulse_start = time.time()
@@ -37,29 +35,41 @@ while True:
     while GPIO.input(R_ECHO)==1:
         right_pulse_end = time.time()
 
+    right_pulse_duration = right_pulse_end - right_pulse_start
+    
+    right_distance = right_pulse_duration * 17150
+    right_distance = round(right_distance, 2)
+    
+    if right_distance > 2 and right_distance < 400:
+        print("RIGHT -- Mesafe:",right_distance - 0.5,"cm")
+    else:
+        print("RIGHT -- Menzil asildi")
+
+    # Rigth sensor end
+    # Left sensor start 
+
+    GPIO.output(L_TRIG, False)
+    print("Olculuyor...")
+    time.sleep(1)
+
+    GPIO.output(L_TRIG, True)
+    time.sleep(0.00001)
+    GPIO.output(L_TRIG, False)
+
     while GPIO.input(L_ECHO)==0:
         left_pulse_start = time.time()
 
     while GPIO.input(L_ECHO)==1:
         left_pulse_end = time.time()
 
-    right_pulse_duration = right_pulse_end - right_pulse_start
-    
     left_pulse_duration = left_pulse_end - left_pulse_start
     
-
-    right_distance = right_pulse_duration * 17150
-    right_distance = round(right_distance, 2)
-
     left_distance = left_pulse_duration * 17150
     left_distance = round(left_distance, 2)
-
-    if right_distance > 2 and right_distance < 400:
-        print("RIGHT -- Mesafe:",right_distance - 0.5,"cm")
-    else:
-        print("RIGHT -- Menzil asildi")
 
     if left_distance > 2 and left_distance < 400:
         print("LEFT -- Mesafe:",left_distance - 0.5,"cm")
     else:
         print("LEFT -- Menzil asildi")
+    
+    # Left sensor end

255c46e85a9a1507723261931c4191f6b0fbf09a
Atakan Yontar <atakanyontar@gmail.com>
2022-05-26 05:28:12 +0300

    feat: demo flask api step3

---
+++ asan_car/rpiWebServer/asancar_api.py
@@ -0,0 +1,21 @@
+import RPi.GPIO as GPIO
+from flask import Flask, render_template, request
+app = Flask(__name__)
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+	
+@app.route("/")
+def index():
+	return 'Engine has started'
+	
+@app.route("/<deviceName>/<action>")
+def action(deviceName, action):
+	if deviceName == 'manual':
+		actuator = 'manual'
+	elif deviceName == 'auto':
+		actuator = 'Obstacle avoiding'
+	else:
+		actuator = 'is confused'
+	return render_template('index.html', actuator)
+if __name__ == "__main__":
+   app.run(host='0.0.0.0', port=80, debug=True)

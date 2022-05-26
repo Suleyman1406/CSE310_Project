19a91fda33069dddc3ba831b40bad5c3e86e25db
Atakan Yontar <atakanyontar@gmail.com>
2022-05-26 12:16:51 +0300

    try asancar api v2

--- try/asancar_api.py
+++ try/asancar_api.py
@@ -1,11 +1,15 @@
 import RPi.GPIO as GPIO
 from flask import Flask, render_template, request
+from flask.ext.cors import CORS, cross_origin
 import time
 from l9119_engine import forward, backward, left, stop, right
 from hcsr04 import calculate_right_distance, calculate_left_distance
 from obstacle_avoiding import obstacle_avoidence
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/foo": {"origins": "*"}})
+app.config['CORS_HEADERS'] = 'Content-Type'
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
     
@@ -14,6 +18,7 @@ def index():
     return 'Engine has started'
     
 @app.route("/<deviceName>/<action>")
+@cross_origin(origin='*',headers=['Content-Type','Authorization'])
 def action(deviceName, action):
     if deviceName == 'manual':
         if action == 'forward':

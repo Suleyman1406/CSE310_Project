1e56f4f3a3601a57822ad9096268fb5ebfaad2d4
Atakan Yontar <atakanyontar@gmail.com>
2022-05-26 05:45:39 +0300

    feat: demo flask api step6

--- asan_car/rpiWebServer/asancar_api.py
+++ asan_car/rpiWebServer/asancar_api.py
@@ -1,5 +1,10 @@
 import RPi.GPIO as GPIO
 from flask import Flask, render_template, request
+import time
+from l9119_engine import forward, backward, left, stop, right
+from hcsr04 import calculate_right_distance, calculate_left_distance
+from obstacle_avoiding import main
+
 app = Flask(__name__)
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
@@ -12,15 +17,15 @@ def index():
 def action(deviceName, action):
     if deviceName == 'manual':
         if action == 'forward':
-                    actuator = 'going forward'
+                    forward()
         elif action == 'backward':
-                    actuator = 'going backward'
+                    backward()
         elif action == 'right':
-                    actuator = 'going right'
+                    right()
         elif action == 'left':
-                    actuator = 'going left'
+                    left()
     elif deviceName == 'auto':
-        actuator = 'Obstacle avoiding'
+        main()
     else:
         actuator = 'is confused'
     return actuator

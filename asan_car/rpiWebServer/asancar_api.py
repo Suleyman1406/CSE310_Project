4d4dfdbd4d194cb5935e5d340cd5d358a9668fb5
Atakan Yontar <atakanyontar@gmail.com>
2022-05-26 05:41:54 +0300

    feat: demo flask api step5

--- asan_car/rpiWebServer/asancar_api.py
+++ asan_car/rpiWebServer/asancar_api.py
@@ -3,19 +3,26 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-	
+    
 @app.route("/")
 def index():
-	return 'Engine has started'
-	
+    return 'Engine has started'
+    
 @app.route("/<deviceName>/<action>")
 def action(deviceName, action):
-	if deviceName == 'manual':
-		actuator = 'manual'
-	elif deviceName == 'auto':
-		actuator = 'Obstacle avoiding'
-	else:
-		actuator = 'is confused'
-	return actuator
+    if deviceName == 'manual':
+        if action == 'forward':
+                    actuator = 'going forward'
+        elif action == 'backward':
+                    actuator = 'going backward'
+        elif action == 'right':
+                    actuator = 'going right'
+        elif action == 'left':
+                    actuator = 'going left'
+    elif deviceName == 'auto':
+        actuator = 'Obstacle avoiding'
+    else:
+        actuator = 'is confused'
+    return actuator
 if __name__ == "__main__":
    app.run(host='0.0.0.0', port=80, debug=True)
